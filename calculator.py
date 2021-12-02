a72637cf86868485ad3c7e56c817d0942a859fbc
Pahan <5972140@gmail.com>
2021-12-02 10:22:04 +0200

    version 3.0.0 added < and >

--- calculator.py
+++ calculator.py
@@ -4,6 +4,7 @@ class Calculator:
         self.num2 = num2
         self.oper = oper
         self.__supported_operations = ("+", "-", "/", "*")
+        self.__supported_compare_operations = ("<", ">")
 
     @property
     def calculation(self):
@@ -11,6 +12,10 @@ class Calculator:
             if self.num1 == 0 and self.oper == "/":
                 return "Division by zero"
             return eval(f"{self.num1}{self.oper}{self.num2}")
+        elif self.oper == "<":
+            return f"{self.num1} bigger than {self.num2}"
+        elif self.oper == ">":
+            return f"{self.num2} bigger than {self.num1}"
         else:
             return "Unexpected format"
 
